9bb7e71d4807928b2aff074197abcd4a37991925
ikue <ikue@DESKTOP-9H2ULG1>
2017-10-28 22:00:01 +0900

    warにするための設定を追記

--- src/main/java/com/example/boot/controller/HelloController.java
+++ src/main/java/com/example/boot/controller/HelloController.java
@@ -1,8 +1,10 @@
 package com.example.boot.controller;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
 @RestController
 public class HelloController {
 
